afdb6284804732399dae6123dc9ad73682e34c2d
prioneto <wot.mits@gmail.com>
2024-03-19 01:53:53 +0100

    Fix typo in intro.tsx

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -44,7 +44,7 @@ export default function Intro() {
 			</div>
 
 			<motion.h1 className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }}>
-				<span className="font-bold">Hello, I'm Dimitris.</span> I'm currently working as a <span className="font-bold">full-stack developer</span> with <span className="font-bold">2 years</span> of experience. I enjoy building <span className="italic">sites, apps & games</span>. My favorite framework is <span className="underline">React (Next.js)</span>.
+				<span className="font-bold">Hello, I&apos;m Dimitris.</span> I&apos;m currently working as a <span className="font-bold">full-stack developer</span> with <span className="font-bold">2 years</span> of experience. I enjoy building <span className="italic">sites, apps & games</span>. My favorite framework is <span className="underline">React (Next.js)</span>.
 			</motion.h1>
 
 			<motion.div
