6e964c7e685ae9ab5ab3fb08b7ceaaa670e53992
prioneto <wot.mits@gmail.com>
2024-03-19 23:38:01 +0100

    Add clsx and tailwind-merge imports, and create cn utility function

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -5,6 +5,9 @@ import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
+import Image from "next/image";
+
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/dialog";
 
 export default function Projects() {
 	const { ref } = useSectionInView("Projects", 0.5);
@@ -12,10 +15,25 @@ export default function Projects() {
 	return (
 		<section ref={ref} id="projects" className="scroll-mt-28 mb-28">
 			<SectionHeading>My projects</SectionHeading>
-			<div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center">
+			<div className="grid grid-cols-1 lg:grid-cols-2 gap-4 items-center">
 				{projectsData.map((project, index) => (
 					<div className="p-4" key={index}>
-						<Project {...project} />
+						<Dialog>
+							<DialogTrigger>
+								<Project {...project} />
+							</DialogTrigger>
+							<DialogContent>
+								<div className="flex flex-col justify-between space-y-4">
+									<DialogHeader>
+										<DialogTitle>{project.title}</DialogTitle>
+									</DialogHeader>
+									<DialogDescription>{project.description}</DialogDescription>
+								</div>
+								<div className="relative h-full w-full hidden lg:block">
+									<Image src={project.imageUrl} alt="Project Image" objectFit="cover" />
+								</div>
+							</DialogContent>
+						</Dialog>
 					</div>
 				))}
 			</div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -21,3 +21,9 @@ export const getErrorMessage = (error: unknown): string => {
 
 	return message;
 };
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs));
+}
