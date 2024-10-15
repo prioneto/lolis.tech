6def252cc519f6d137ea4cd3df7f247e62dff2c5
Developers-Note-Bridge <wot.mits@gmail.com>
2024-10-15 20:29:17 +0200

    Refactor about component and update content

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -19,11 +19,10 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        As a computer science student, I have had the opportunity to work on multiple team projects throughout my degree. These experiences have taught me the value of effective communication,
-        collaboration, and the importance of meeting project deadlines. In addition to my academic pursuits, I have 3 years of experience in C# and Unity, allowing me to delve into the world of game
-        development. At the same time the last 2 years I have been working as a Full-Stack Developer and specifically with Next.js and Vercel. My experience has honed my problem-solving skills and
-        helped me develop a keen eye for detail. I take pride in my ability to work well under pressure, which has led me to be competitive and excel at teamwork. Overall, I am passionate about the
-        intersection of technology and creativity, and am excited to continue my growth and development as a computer science professional.
+        As a computer science student, I’ve worked on multiple team projects, gaining valuable experience in communication, collaboration, and meeting deadlines. I have 3 years of experience with C#
+        and Unity, which sparked my passion for game development. Over the past 2 years, I&apos;ve focused on Full-Stack Development, specifically with Next.js and Vercel, further refining my
+        problem-solving skills and attention to detail. I thrive under pressure and excel in teamwork, and I’m excited to continue growing at the intersection of technology and creativity as I pursue
+        my career in computer science.
       </p>
     </motion.section>
   );
