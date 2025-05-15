5c260e5b08bb925c6d9197265011b629190550ce
Dimitris Lolis <wot.mits@gmail.com>
2025-05-15 16:01:10 +0200

    Format code for improved readability and consistency

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -10,33 +10,33 @@ import Image from "next/image";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/dialog";
 
 export default function Projects() {
-	const { ref } = useSectionInView("Projects", 0.5);
+  const { ref } = useSectionInView("Projects", 0.5);
 
-	return (
-		<section ref={ref} id="projects" className="scroll-mt-28 mb-28">
-			<SectionHeading>My projects</SectionHeading>
-			<div className="grid grid-cols-1 lg:grid-cols-2 gap-4 items-center">
-				{projectsData.map((project, index) => (
-					<div className="p-4" key={index}>
-						<Dialog>
-							<DialogTrigger>
-								<Project {...project} />
-							</DialogTrigger>
-							<DialogContent>
-								<div className="flex flex-col justify-between space-y-4">
-									<DialogHeader>
-										<DialogTitle>{project.title}</DialogTitle>
-									</DialogHeader>
-									<DialogDescription>{project.description}</DialogDescription>
-								</div>
-								<div className="relative h-full w-full hidden lg:block">
-									<Image src={project.imageUrl} alt="Project Image" objectFit="cover" />
-								</div>
-							</DialogContent>
-						</Dialog>
-					</div>
-				))}
-			</div>
-		</section>
-	);
+  return (
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+      <SectionHeading>My projects</SectionHeading>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 items-center">
+        {projectsData.map((project, index) => (
+          <div className="p-4" key={index}>
+            <Dialog>
+              <DialogTrigger>
+                <Project {...project} />
+              </DialogTrigger>
+              <DialogContent>
+                <div className="flex flex-col justify-between space-y-4">
+                  <DialogHeader>
+                    <DialogTitle>{project.title}</DialogTitle>
+                  </DialogHeader>
+                  <DialogDescription>{project.description}</DialogDescription>
+                </div>
+                <div className="relative h-full w-full hidden lg:block">
+                  <Image src={project.imageUrl} alt="Project Image" objectFit="cover" />
+                </div>
+              </DialogContent>
+            </Dialog>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
